e8e9b09fd98d3a1c0547337df872352431599e47
Waleed Javed <wahajiqbal78@gmail.com>
2022-04-04 02:12:57 +0500

    cleaning code

---
+++ index.js
@@ -0,0 +1,12 @@
+var input = document.getElementById("input");
+var message = document.getElementById("fas")[0];
+var animationTime = 400;
+input.addEventListener("focus", function (){
+    message.style.display = "none";
+    message.style.transition = `cubic-bezier(0.68, -0.55, 0.27, 1.55)${animationTime}ms`;
+});
+input.addEventListener("focusout", function (){
+    setTimeout(()=>{
+        message.style.display = "flex;"
+    }, animationTime );
+});
